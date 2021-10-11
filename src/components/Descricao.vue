30062866bb465b0447ef2a5919bef2d33c32489e
ecrseer <juustino@live.com>
2021-10-10 22:04:54 -0300

    radio ok

--- src/components/Descricao.vue
+++ src/components/Descricao.vue
@@ -7,79 +7,27 @@
         id="meusprojetos"
       >
         <h6 class="d-block text-center">Projetos</h6>
-        <div class="pullc border bs rounded" :style="estilizacaoPgLibfy">
-          <input
-            type="radio"
-            id=""
-            value="libfy"
-            class="pgRadio invisibleRadio"
-            v-on:click="checkUncheck($event.target.value)"
-            v-model="selectPg"
-            v-if="selectPg !== 'libfy'"
-          />
-          <div v-if="selectPg === 'libfy'">
-            <input
-              type="radio"
-              id=""
-              value="libfy"
-              v-on:click="checkUncheck($event.target.value)"
-              v-model="selectPg"
-              class="invisibleRadio fas fa-window-close"
-            />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut qui
-              alias, eum nesciunt, distinctio suscipit expedita perferendis nisi
-              pariatur odit provident. Placeat, beatae? Suscipit fuga non id
-              accusamus quis nisi?
-            </p>
-          </div>
-        </div>
+   
         <RadioContainer v-bind:selectPg="selectPg"
          v-bind:pagina='"libfy"'></RadioContainer>
 
+        <RadioContainer v-bind:selectPg="selectPg"
+         v-bind:pagina='"gdrive"'></RadioContainer>
 
+        <RadioContainer v-bind:selectPg="selectPg"
+         v-bind:pagina='"starwr"'></RadioContainer>
 
-
-
-         
-        <div class="pullc border bs rounded" :style="imgLibfy">
-          <input
-            type="radio"
-            id=""
-            value="xab"
-            class="invisibleRadio"
-            v-on:click="checkUncheck($event.target.value)"
-            v-model="selectPg"
-            data-toggle="collapse"
-            data-target="#collapseExample"
-          />
-          <div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut qui
-              alias, eum nesciunt, distinctio suscipit expedita perferendis nisi
-              pariatur odit provident. Placeat, beatae? Suscipit fuga non id
-              accusamus quis nisi?
-            </p>
-          </div>
-        </div>
-        <!--  <input
-          type="radio"
-          id=""
-          value="tre"
-          class="pullc border bs rounded"
-          :style="imgLibfy"
-          v-on:click="checkUncheck($event.target.value)"
-          v-model="selectPg"
-        /> -->
+        <RadioContainer v-bind:selectPg="selectPg"
+         v-bind:pagina='"ytime"'></RadioContainer>
 
         <a href="http://time2shareyt.vercel.app/" class="">
-          <div :style="imgYt" class="pullc border bs rounded"></div
-        ></a>
+          b
+        </a>
         <a href="https://gd-sheet-y2.vercel.app/">
-          <div class="pullc border bs rounded" :style="imgGd"></div>
+          b
         </a>
         <a href="https://g4br13l-ju5t1n0.vercel.app/">
-          <div class="pullc border bs rounded" :style="imgSw"></div>
+          b
         </a>
       </section>
 
@@ -112,84 +60,16 @@ import RadioContainer from './RadioContainer.vue';
 export default {
   components: { RadioContainer },
   name: "Descricao",
-  data() {
-    /* eslint-disable */
-    let imgYt, imgGd, imgSw, imgLibfy;
-
-    let baseBackgrnd = (img) => {
-      let requestImg = require(`@/assets/${img}`);
-      return { "background-image": `url(${requestImg})` };
-    };
-    imgGd = baseBackgrnd("gdrive.png");
-    imgYt = baseBackgrnd("ytime.png");
-    imgSw = baseBackgrnd("starwr.png");
-    imgLibfy = baseBackgrnd("libfy.png");
-
-    return {
-      imgGd,
-      imgYt,
-      imgSw,
-      imgLibfy,
+  data() { 
+    return { 
       selectPg: "",
     };
-  },
-  props: {
-    msg: String,
-  },
-  methods: {
-    /* eslint-disable */
-    checkUncheck(val) {
-      if (this.selectPg === val) {
-        this.selectPg = "";
-      }
-    },
-    baseBackgrnd(img) {
-      let requestImg = require(`@/assets/${img}`);
-      return { "background-image": `url(${requestImg})` };
-    },
-  },
-  computed: {
-    estilizacaoPgLibfy() {
-      let alturaPgExpandida = { height: "100vh" };
-      let fundo = this.baseBackgrnd("libfy.png");
-      let estilizacaoPgLibfy = {
-        ...fundo,
-        ...(this.selectPg === "libfy" ? alturaPgExpandida : {}),
-      };
+  }, 
 
-      return estilizacaoPgLibfy;
-    },
-  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.bs {
-  box-shadow: inset 0 0 0 1000px rgb(224 237 237 / 87%);
-}
-.pullc {
-  width: 100%;
-  height: 13vh;
-  background-position-x: center;
-  background-position-y: center;
-  transition: 3s all, 1s height, 2s box-shadow;
-}
-.pgRadio {
-  width: 100%;
-  height: 100%;
-}
-.invisibleRadio {
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  border: none;
-}
-.pullc:hover {
-  height: 28vh;
-  box-shadow: inset 0 0 0 1000px rgb(224 237 237 / 42%);
-}
-.pullc:checked {
-  box-shadow: inset 0 0 0 1000px rgb(1 1 1 / 92%);
-}
+ 
 </style>
